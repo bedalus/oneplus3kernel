932b449859b0ac5d293e80e81769e03da97f556d
Amit Pundir <amit.pundir@linaro.org>
2017-04-06 15:05:51 +0530

    Revert "Don't kill IPv4 sockets when killing IPv6 sockets was requested."
    This reverts commit 70f767ae180a8b7ab0058145ed5375bc357a39a6.
    
    Now that SOCK_DESTROY is in place, lets revert SIOCKILLADDR patches.
    
    Change-Id: I0d2675ca598686cad0b9a4964c44df8d7945bed1
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -3288,8 +3288,6 @@ restart:
 #if defined(CONFIG_IPV6)
 			if (family == AF_INET6) {
 				struct in6_addr *s6;
-				if (!inet->pinet6)
-					continue;
 
 				if (sk->sk_family == AF_INET)
 					continue;
